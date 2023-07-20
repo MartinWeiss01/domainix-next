7cc4eb06e078c5b1971bc23952ae54389f4df865
Martin Weiss <jsem@martinweiss.cz>
2023-07-20 22:46:05 +0200

    feat(List): display registrars logo

--- app/registrars/page.tsx
+++ app/registrars/page.tsx
@@ -7,9 +7,10 @@ export default function RegistrarsPage() {
   const domainixData = use(getDomainixData())
 
   return (
-    <div>
-      <h1>Registrars page</h1>
-      <RegistrarsList registrars={domainixData.registrars} />
+    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div className="px-4 py-8 sm:px-0">
+        <RegistrarsList registrars={domainixData.registrars} />
+      </div>
     </div>
   )
 }
--- components/Registrars/List.tsx
+++ components/Registrars/List.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Registrar } from "@/types/apiResponse"
+import Link from "next/link"
 
 interface RegistrarsListProps {
   registrars: Registrar[]
@@ -9,14 +10,21 @@ interface RegistrarsListProps {
 const RegistrarsList = ({ registrars }: RegistrarsListProps) => {
   return (
     <div>
-      <h2>Registrars List Component</h2>
-      <ul>
+      <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-16 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
         {registrars.map(registrar => (
-          <li key={registrar.id}>
-            <a href={`registrars/${registrar.slug}`}>{registrar.name} - {JSON.stringify(registrar)}</a>
-          </li>
+          <div className="shadow p-2">
+            <Link href={`registrars/${registrar.slug}`} key={registrar.id}>
+              <img
+                className="col-span-4 max-h-16 w-full object-contain lg:col-span-1"
+                src={registrar.img}
+                alt={registrar.name}
+                width={158}
+                height={48}
+              />
+            </Link>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
