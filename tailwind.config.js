577761c88c250911497403387b426d8ec35ed64d
Martin Weiss <jsem@martinweiss.cz>
2023-07-21 16:46:14 +0200

    ci(tailwind): add custom xs breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        'xs': '425px',
+      }
     },
   },
   plugins: [],
