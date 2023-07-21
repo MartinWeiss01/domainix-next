4203a6dbd97ba98c2720fffa4cfb226200061c35
Martin Weiss <jsem@martinweiss.cz>
2023-07-21 12:13:54 +0200

    feat(Estimation): change Form & Table design

--- components/Home/Form.tsx
+++ components/Home/Form.tsx
@@ -19,15 +19,15 @@ const Form = ({ availableTLDs, findAction, processing }: FormProps) => {
   const [formData, setFormData] = useState(INITIAL_FORM_STATE)
 
   return (
-    <div className="bg-red-400 flex flex-col max-w-[400px] p-3 space-y-2">
-      <h2>Search</h2>
+    <div className="flex flex-col p-6 space-y-3 bg-gray-50 rounded-lg">
+      <h2 className="font-bold text-2xl">Search</h2>
 
-      <div className="flex flex-col bg-red-500">
-        <h2>Domain Name</h2>
+      <div className="flex flex-col space-y-1">
+        <h2 className="block text-sm font-medium leading-6 text-gray-900">Domain Name</h2>
 
         <div className="flex w-full">
-          <input className="px-2 py-4 text-sm md:p-4 md:font-semibold bg-white text-gray-900 outline-0 w-full" value={formData.domain} onChange={(e) => setFormData(prev => ({ ...prev, domain: e.target.value }))} type="text" placeholder="absolutelyuniquedomain" />
-          <input className="px-2 py-4 text-sm md:p-4 md:font-semibold bg-black text-white outline-0 w-full" value={formData.tld} onChange={(e) => setFormData(prev => ({ ...prev, tld: e.target.value }))} list="available-tlds" id="tld" name="tld" placeholder=".com" />
+          <input className="px-3 py-4 text-sm md:p-3 md:font-semibold bg-white text-gray-900 outline-0 w-full rounded-l" value={formData.domain} onChange={(e) => setFormData(prev => ({ ...prev, domain: e.target.value }))} type="text" placeholder="absolutelyuniquedomain" />
+          <input className="px-3 py-4 text-sm md:p-3 md:font-semibold bg-gray-100 text-black outline-0 w-[164px] rounded-r" value={formData.tld} onChange={(e) => setFormData(prev => ({ ...prev, tld: e.target.value }))} list="available-tlds" id="tld" name="tld" placeholder=".com" />
           <datalist id="available-tlds">
             {availableTLDs.map(el => (
               <option value={el} key={el} />
@@ -36,23 +36,24 @@ const Form = ({ availableTLDs, findAction, processing }: FormProps) => {
         </div>
       </div>
 
-      <div className="flex flex-col bg-red-600">
-        <h2>Years</h2>
+      <div className="flex flex-col space-y-1">
+        <h2 className="block text-sm font-medium leading-6 text-gray-900">Years</h2>
 
         <div className="flex items-center space-x-3 w-full">
-          <input className="w-full" type="range" min="1" max="10" value={formData.years} onChange={(e) => setFormData(prev => ({ ...prev, years: parseInt(e.target.value) }))} />
-          <div className="bg-blue-200 w-9 h-9 flex items-center justify-center">
+          <input className="w-full accent-primary-600" type="range" min="1" max="10" value={formData.years} onChange={(e) => setFormData(prev => ({ ...prev, years: parseInt(e.target.value) }))} />
+          <div className="border flex items-center p-1 w-9 h-9 justify-center font-semibold uppercase text-sm">
             {formData.years}
           </div>
         </div>
       </div>
 
-      {processing && <p>Processing...</p>}
-      <button className="p-4 bg-black text-white" onClick={() => findAction(formData)} disabled={processing}>
-        {
-          processing ? 'Processing...' : 'Find Registrars'
-        }
-      </button>
+      <div className="flex justify-end">
+        <button className="py-3 px-6 bg-black text-white text-sm font-semibold rounded" onClick={() => findAction(formData)} disabled={processing}>
+          {
+            processing ? 'Processing...' : 'Find Registrars'
+          }
+        </button>
+      </div>
     </div>
   )
 }
--- components/Home/Table.tsx
+++ components/Home/Table.tsx
@@ -2,6 +2,7 @@
 
 import { useCart } from "@/store/cart"
 import { EstimationData } from "@/types/estimation"
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 
 interface TableProps {
   estimationData: EstimationData[]
@@ -22,37 +23,101 @@ const Table = ({ estimationData, processing, domainName, years }: TableProps) =>
   }
 
   return (
-    <div className="bg-blue-400">
-      <h2>Table Component</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>RName</th>
-            <th>date</th>
-            <th>domain</th>
-            <th>priceReg</th>
-            <th>priceRen</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {estimationData.map(el => (
-            <tr key={el.registrar.id}>
-              <td>{el.registrar.name}</td>
-              <td>{el.date}</td>
-              <td>{el.detail.domain}</td>
-              <td>{el.detail.priceReg}</td>
-              <td>{el.detail.priceRen}</td>
-              <td>
-                <button onClick={() => handleAddToCart(el)}>
-                  Add to Cart
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {processing && <p>Processing...</p>}
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="overflow-hidden rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Registrar
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Domain Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Registration Price
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Renewal Price
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Total Price
+                  </th>
+
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    <span className="sr-only">Add to cart</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {estimationData.map(el => (
+                  <tr key={el.registrar.id}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="flex-shrink-0 h-10 w-10">
+                          <img
+                            className="h-10 w-10 rounded-full"
+                            src=""
+                            alt=""
+                          />
+                        </div>
+                        <div className="ml-4">
+                          <div className="text-sm font-medium text-gray-900">
+                            {el.registrar.name}
+                          </div>
+                          <small className="text-gray-500">
+                            {new Date(el.date).toLocaleDateString()}
+                          </small>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{domainName}{el.detail.domain}</div>
+                      <div className="text-sm text-gray-500">Test</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {el.detail.priceReg} CZK/year
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {el.detail.priceRen} CZK/year
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {el.detail.priceReg + (years - 1) * el.detail.priceRen} CZK
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        <button onClick={() => handleAddToCart(el)}>
+                          <ShoppingCartIcon className="h-5 w-5" />
+                        </button>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
