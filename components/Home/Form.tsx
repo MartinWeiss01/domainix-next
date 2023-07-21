4ca4ec62422245717acd69e242b970b86d671193
Martin Weiss <jsem@martinweiss.cz>
2023-07-21 17:20:53 +0200

    feat(Form): improve Form responsiveness

--- components/Home/Form.tsx
+++ components/Home/Form.tsx
@@ -23,11 +23,20 @@ const Form = ({ availableTLDs, findAction, processing }: FormProps) => {
       <h2 className="font-bold text-2xl">Search</h2>
 
       <div className="flex flex-col space-y-1">
-        <h2 className="block text-sm font-medium leading-6 text-gray-900">Domain Name</h2>
 
-        <div className="flex w-full">
-          <input className="px-3 py-4 text-sm md:p-3 md:font-semibold bg-white text-gray-900 outline-0 w-full rounded-l" value={formData.domain} onChange={(e) => setFormData(prev => ({ ...prev, domain: e.target.value }))} type="text" placeholder="absolutelyuniquedomain" />
-          <input className="px-3 py-4 text-sm md:p-3 md:font-semibold bg-gray-100 text-black outline-0 w-[164px] rounded-r" value={formData.tld} onChange={(e) => setFormData(prev => ({ ...prev, tld: e.target.value }))} list="available-tlds" id="tld" name="tld" placeholder=".com" />
+        <label htmlFor="domain" className="xs:block text-sm font-medium leading-6 text-gray-900 hidden">
+          Domain Name
+        </label>
+        <div className="flex flex-col xs:flex-row w-full space-y-2 xs:space-y-0">
+          <label htmlFor="domain" className="block text-sm font-medium leading-6 text-gray-900 xs:hidden">
+            Domain Name
+          </label>
+          <input name="domain" id="domain" className="border border-gray-100 xs:border-0 px-3 py-4 text-sm md:font-semibold bg-white text-gray-900 outline-0 w-full rounded xs:rounded-none xs:rounded-l" value={formData.domain} onChange={(e) => setFormData(prev => ({ ...prev, domain: e.target.value }))} type="text" placeholder="absolutelyuniquedomain" />
+
+          <label htmlFor="tld" className="block text-sm font-medium leading-6 text-gray-900 xs:hidden">
+            TLD
+          </label>
+          <input name="tld" id="tld" className="border border-gray-100 xs:border-0 px-3 py-4 text-sm md:font-semibold bg-gray-100 text-black outline-0 w-full xs:w-[164px] rounded xs:rounded-none xs:rounded-r" value={formData.tld} onChange={(e) => setFormData(prev => ({ ...prev, tld: e.target.value }))} list="available-tlds" id="tld" name="tld" placeholder=".com" />
           <datalist id="available-tlds">
             {availableTLDs.map(el => (
               <option value={el} key={el} />
@@ -48,7 +57,7 @@ const Form = ({ availableTLDs, findAction, processing }: FormProps) => {
       </div>
 
       <div className="flex justify-end">
-        <button className="py-3 px-6 bg-black text-white text-sm font-semibold rounded" onClick={() => findAction(formData)} disabled={processing}>
+        <button className="w-full sm:w-auto py-3 px-5 bg-gray-900 hover:bg-black transition-colors text-white text-sm font-semibold rounded" onClick={() => findAction(formData)} disabled={processing}>
           {
             processing ? 'Processing...' : 'Find Registrars'
           }
