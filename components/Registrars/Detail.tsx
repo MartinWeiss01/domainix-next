5eff0e51fd8899f4e1fa3c29e65a65780b257a1d
Martin Weiss <jsem@martinweiss.cz>
2023-07-21 16:16:10 +0200

    feat(Detail): improve layout & responsiveness
    Table of Available TLDs is now responsive and, limited height and sticky header to make it more user friendly

--- components/Registrars/Detail.tsx
+++ components/Registrars/Detail.tsx
@@ -9,45 +9,39 @@ interface RegistrarDetailProps {
 const RegistrarDetail = ({ data }: RegistrarDetailProps) => {
   return (
     <>
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              TLD
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              Registration Price
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              Renewal Price
-            </th>
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
-          {data?.domains?.map(domain => (
-            <tr key={domain.domain}>
-              <td className="px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-gray-900">{domain.domain}</div>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {domain.priceReg} CZK/year
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {domain.priceRen} CZK/year
-              </td>
+      <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight my-4">Available TLDs</h2>
+      <div className="overflow-x-auto max-h-96">
+        <table className="min-w-full divide-y divide-gray-200 bg-gray-50 relative">
+          <thead className="bg-gray-100 sticky top-0">
+            <tr>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                TLD
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Registration Price
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Renewal Price
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {data?.domains?.map(domain => (
+              <tr key={domain.domain}>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-900 font-medium">{domain.domain}</div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {domain.priceReg} CZK/year
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {domain.priceRen} CZK/year
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
