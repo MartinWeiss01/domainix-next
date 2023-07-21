0427e765d69d84644a66233235c5bad6d27f22e1
Martin Weiss <jsem@martinweiss.cz>
2023-07-21 23:06:39 +0200

    feat(List): improve grid layout

--- app/registrars/page.tsx
+++ app/registrars/page.tsx
@@ -9,6 +9,7 @@ export default function RegistrarsPage() {
   return (
     <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="px-4 py-8 sm:px-0">
+        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Available Registrars</h2>
         <RegistrarsList registrars={domainixData.registrars} />
       </div>
     </div>
--- components/Registrars/List.tsx
+++ components/Registrars/List.tsx
@@ -10,12 +10,12 @@ interface RegistrarsListProps {
 const RegistrarsList = ({ registrars }: RegistrarsListProps) => {
   return (
     <div>
-      <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-16 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+      <div className="mx-auto mt-10 grid max-w-lg grid-cols-2 items-center gap-x-2 gap-y-6 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 md:gap-y-10 md:grid-cols-4 md:max-w-3xl lg:mx-0 lg:max-w-none lg:grid-cols-5">
         {registrars.map(registrar => (
-          <div className="shadow p-2">
+          <div key={registrar.id} className="p-2">
             <Link href={`registrars/${registrar.slug}`} key={registrar.id}>
               <img
-                className="col-span-4 max-h-16 w-full object-contain lg:col-span-1"
+                className="col-span-4 max-h-10 md:max-h-16 w-full object-contain lg:col-span-1 scale-90 hover:scale-100 transition duration-300 ease-in-out"
                 src={registrar.img}
                 alt={registrar.name}
                 width={158}
