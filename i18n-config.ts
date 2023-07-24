c21a1d1a06480338f9b18c4c961ab42a554d2ece
Martin Weiss <jsem@martinweiss.cz>
2023-07-24 16:08:25 +0200

    feat(i18n): Navbar i18n implementation

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,6 +1,18 @@
 export const i18n = {
   defaultLocale: 'en',
   locales: ['en', 'cs'],
+  navbar: [
+    {
+      id: 'en',
+      name: 'English',
+      img: 'https://flagcdn.com/gb.svg',
+    },
+    {
+      id: 'cs',
+      name: 'čeština',
+      img: 'https://flagcdn.com/cz.svg',
+    },
+  ]
 } as const
 
 export type Locale = (typeof i18n)['locales'][number]
