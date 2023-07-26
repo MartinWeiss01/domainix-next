886a2f0e76009681a4099c150015add2c04f7a89
Martin Weiss <jsem@martinweiss.cz>
2023-07-26 14:34:07 +0200

    ci(store): change the behavior of VAT controllers
    If the user sets the VAT value to 0, store will automatically exclude VAT
    Also, if the VAT value is set to 0 and user attempts to include VAT, the VAT value will change to the default (non-zero) value

--- store/vat.ts
+++ store/vat.ts
@@ -7,9 +7,27 @@ interface IVAT {
   setIncludeVAT: (includeVAT: boolean) => void
 }
 
+const DEFAULT_VAT_VALUE = 21
+const DEFAULT_INCLUDE_VAT = true
+
 export const useVAT = create<IVAT>(set => ({
-  vat: 21,
-  includeVAT: true,
-  setVAT: (vat: number) => set({ vat }),
-  setIncludeVAT: (includeVAT: boolean) => set({ includeVAT })
+  vat: DEFAULT_VAT_VALUE,
+  includeVAT: DEFAULT_INCLUDE_VAT,
+  setVAT: (vat: number) => set(state => {
+    if (vat < 0 || vat > 100) return state
+    else {
+      return {
+        vat,
+        includeVAT: vat !== 0
+      }
+    }
+  }),
+  setIncludeVAT: (includeVAT: boolean) => set(state => {
+    if (includeVAT && state.vat === 0) {
+      return {
+        vat: DEFAULT_VAT_VALUE,
+        includeVAT
+      }
+    } else return { includeVAT }
+  })
 }))
