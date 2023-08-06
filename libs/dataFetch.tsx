fb31b2a2d7a0480e9fb696632325707fc931334b
Martin Weiss <jsem@martinweiss.cz>
2023-08-06 13:29:24 +0200

    ci(api): add CurrencyAPI endpoint

--- libs/dataFetch.tsx
+++ libs/dataFetch.tsx
@@ -1,5 +1,6 @@
 import 'server-only'
 import { APIResponse } from "@/types/apiResponse"
+import { ICurrenciesResponse } from '@/types/currenciesApiResponse'
 
 export async function getDomainixData() {
   const res = await fetch(process.env.NEXT_PUBLIC_DOMAINIX_API_URL, { next: { revalidate: 21600 } })
@@ -8,5 +9,15 @@ export async function getDomainixData() {
   }
 
   const data: APIResponse = await res.json()
+  return data
+}
+
+export async function getCurrenciesData() {
+  const res = await fetch(process.env.NEXT_PUBLIC_CURRENCY_API_URL, { next: { revalidate: 86400 } })
+  if (!res.ok) {
+    throw new Error(res.statusText)
+  }
+
+  const data: ICurrenciesResponse = await res.json()
   return data
 }
