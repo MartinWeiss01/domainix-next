ca704cc55e70734d77945efdd0fe621819d146c1
Martin Weiss <jsem@martinweiss.cz>
2023-08-07 13:50:14 +0200

    style(Footer): remove unnecessary elements

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -2,25 +2,16 @@ import 'server-only'
 import Link from "next/link"
 import { ITranslationsFooter } from '@/types/translations'
 
-export default function Footer({
-  translations
-}: {
-  translations: ITranslationsFooter
-}) {
+export default function Footer({ translations }: { translations: ITranslationsFooter }) {
   return (
-    <footer>
-      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-        <div className="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left flex flex-col sm:flex-row justify-between flex-wrap">
-          <Link href="https://martinweiss.cz/" target="_blank">
-            <span>
-              &copy; {new Date().getFullYear()} Martin Weiss
-            </span>
-          </Link>
-          <Link href="https://martinweiss.cz/" target="_blank">
-            <span>{translations.madeWithLove}</span>
-          </Link>
-        </div>
-      </div>
+    <footer className="flex flex-col justify-between flex-wrap max-w-3xl mx-auto px-4 py-8 text-sm text-center border-t border-gray-200 text-gray-500 sm:flex-row sm:px-6 lg:px-8 lg:max-w-7xl">
+      <Link href="https://martinweiss.cz/" target="_blank">
+        <span>&copy; {new Date().getFullYear()} Martin Weiss</span>
+      </Link>
+
+      <Link href="https://martinweiss.cz/" target="_blank">
+        <span>{translations.madeWithLove}</span>
+      </Link>
     </footer>
   )
 }
