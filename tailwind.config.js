61890d908a7a7f4e3ebdd5d8767143394aa5408d
Martin Weiss <jsem@martinweiss.cz>
2023-08-08 21:56:20 +0200

    feat(loading): add keyframes for loading spinner

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,43 @@ module.exports = {
       },
       screens: {
         'xs': '425px',
+      },
+      keyframes: {
+        'loading-animation': {
+          '0%': {
+            top: '36px',
+            left: '36px',
+            width: '0',
+            height: '0',
+            opacity: '0'
+          },
+          '4.9%': {
+            top: '36px',
+            left: '36px',
+            width: '0',
+            height: '0',
+            opacity: '0'
+          },
+          '5%': {
+            top: '36px',
+            left: '36px',
+            width: '0',
+            height: '0',
+            opacity: '1'
+          },
+          '100%': {
+            top: '0px',
+            left: '0px',
+            width: '72px',
+            height: '72px',
+            opacity: '0'
+          }
+        }
       }
     },
+    animation: {
+      'loading-animation': 'loading-animation 1s cubic-bezier(0, 0.2, 0.8, 1) infinite'
+    }
   },
   plugins: [],
 }
