565df7293d3d0a14ede07add66ddae8a3e4b1bf6
Martin Weiss <jsem@martinweiss.cz>
2023-08-15 01:06:03 +0200

    refactor(Table): improve semantics & readability

--- components/Home/Table.tsx
+++ components/Home/Table.tsx
@@ -37,143 +37,114 @@ const Table = ({ estimationData, processing, domainName, years, translations }: 
 
   return (
     <section className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="overflow-x-auto rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200 relative">
-              <thead className="bg-gray-50 sticky top-0">
-                <tr>
-                  <th
-                    scope="col"
-                    className=""
-                  >
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    {translations.colDomain}
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    <div className="flex flex-col">
-                      {translations.colRegPrice}
-                      <small>
-                        {translations.priceDuration}
-                      </small>
-                    </div>
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    <div className="flex flex-col">
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200 relative">
+          <thead className="bg-gray-50 sticky top-0">
+            <tr className="text-left text-xs text-gray-500 uppercase tracking-wider">
+              <th className="hidden sm:flex">{/* Registrar Logo Column */}</th>
 
-                      {translations.colRenPrice}
-                      <small>
-                        {translations.priceDuration}
-                      </small>
-                    </div>
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    {translations.colTotalPrice}
-                  </th>
+              {/* Domain Name Column */}
+              <th className="px-6 py-3 font-medium" scope="col">
+                {translations.colDomain}
+              </th>
 
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    <span className="sr-only">{translations.colAddToCart}</span>
-                  </th>
+              {/* Registration Price Column */}
+              <th className="px-6 py-3 font-medium" scope="col">
+                <div className="flex flex-col">
+                  {translations.colRegPrice}
+                  <small>{translations.priceDuration}</small>
+                </div>
+              </th>
+
+              {/* Renewal Price Column */}
+              <th className="px-6 py-3 font-medium" scope="col">
+                <div className="flex flex-col">
+                  {translations.colRenPrice}
+                  <small>{translations.priceDuration}</small>
+                </div>
+              </th>
+
+              {/* Total Price Column */}
+              <th className="px-6 py-3 font-medium" scope="col">
+                {translations.colTotalPrice}
+              </th>
+
+              <th className="px-6 py-3 text-right font-medium" scope="col">
+                <span className="sr-only">{translations.colAddToCart}</span>
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className="bg-white divide-y divide-gray-200">
+            {currentRows.map(el => {
+              const prepareTotalPrice = (el.detail.priceReg + (years - 1) * el.detail.priceRen)
+              const regPrice = calculatePrice(convertPriceCurrency(el.detail.priceReg, el.registrar.currency, selectedCurrency, currencies), includeVAT, vat)
+              const renPrice = calculatePrice(convertPriceCurrency(el.detail.priceRen, el.registrar.currency, selectedCurrency, currencies), includeVAT, vat)
+              const totalPrice = calculatePrice(convertPriceCurrency(prepareTotalPrice, el.registrar.currency, selectedCurrency, currencies), includeVAT, vat)
+              const regPriceOriginal = calculatePrice(el.detail.priceReg, includeVAT, vat)
+              const renPriceOriginal = calculatePrice(el.detail.priceRen, includeVAT, vat)
+              const totalPriceOriginal = calculatePrice(prepareTotalPrice, includeVAT, vat)
+
+              return (
+                <tr key={el.registrar.id}>
+                  {/* Registrar Logo Column */}
+                  <td className="hidden px-6 sm:flex">
+                    <div title={`${el.registrar.name} (${new Date(el.date).toLocaleDateString()})`} className="flex-shrink-0 h-16 w-16">
+                      <Link href={getLocalizedURL(`/registrars/${el.registrar.slug}`)}>
+                        <img
+                          className="object-scale-down object-center h-16 w-16"
+                          src={el.registrar.img}
+                          alt={el.registrar.name}
+                        />
+                      </Link>
+                    </div>
+                  </td>
+
+                  {/* Domain Name Column */}
+                  <td className="px-6 py-4 max-w-[255px] break-words">
+                    <div className="flex flex-col">
+                      <span className="text-sm text-gray-900">{domainName}{el.detail.domain}</span>
+                      <Link href={getLocalizedURL(`/registrars/${el.registrar.slug}`)}>
+                        <span className="text-sm text-primary-600 hover:text-primary-900">{el.registrar.name}</span>
+                      </Link>
+                    </div>
+                  </td>
+
+                  {/* Registration Price Column */}
+                  <td className="px-6 py-4 text-sm text-gray-500">
+                    <div className="flex flex-col">
+                      <span>{regPrice} {translations[`currency${selectedCurrency}`]}</span>
+                      {el.registrar.currency !== selectedCurrency && <small>{regPriceOriginal} {translations[`currency${el.registrar.currency}`]}</small>}
+                    </div>
+                  </td>
+
+                  {/* Renewal Price Column */}
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    <div className="flex flex-col">
+                      <span>{renPrice} {translations[`currency${selectedCurrency}`]}</span>
+                      {el.registrar.currency !== selectedCurrency && <small>{renPriceOriginal} {translations[`currency${el.registrar.currency}`]}</small>}
+                    </div>
+                  </td>
+
+                  {/* Total Price Column */}
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    <div className="flex flex-col">
+                      <span>{totalPrice} {translations[`currency${selectedCurrency}`]}</span>
+                      {el.registrar.currency !== selectedCurrency && <small>{totalPriceOriginal} {translations[`currency${el.registrar.currency}`]}</small>}
+                    </div>
+                  </td>
+
+                  {/* Add to Cart Column */}
+                  <td className="px-6 py-4 text-right text-sm font-medium">
+                    <button onClick={() => handleAddToCart(el)} className="text-primary-600 hover:text-primary-900">
+                      <PlusIcon className="h-5 w-5" />
+                    </button>
+                  </td>
                 </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {currentRows.map(el => {
-                  const prepareTotalPrice = (el.detail.priceReg + (years - 1) * el.detail.priceRen)
-                  const regPrice = calculatePrice(convertPriceCurrency(el.detail.priceReg, el.registrar.currency, selectedCurrency, currencies), includeVAT, vat)
-                  const renPrice = calculatePrice(convertPriceCurrency(el.detail.priceRen, el.registrar.currency, selectedCurrency, currencies), includeVAT, vat)
-                  const totalPrice = calculatePrice(convertPriceCurrency(prepareTotalPrice, el.registrar.currency, selectedCurrency, currencies), includeVAT, vat)
-                  const regPriceOriginal = calculatePrice(el.detail.priceReg, includeVAT, vat)
-                  const renPriceOriginal = calculatePrice(el.detail.priceRen, includeVAT, vat)
-                  const totalPriceOriginal = calculatePrice(prepareTotalPrice, includeVAT, vat)
-
-                  return (
-                    <tr key={el.registrar.id}>
-                      <td className="px-6 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <div
-                            title={`${el.registrar.name} (${new Date(el.date).toLocaleDateString()})`}
-                            className="flex-shrink-0 h-16 w-16"
-                          >
-                            <Link href={getLocalizedURL(`/registrars/${el.registrar.slug}`)}>
-                              <img
-                                className="object-scale-down object-center h-16 w-16"
-                                src={el.registrar.img}
-                                alt={el.registrar.name}
-                              />
-                            </Link>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">{domainName}{el.detail.domain}</div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        <div className="flex flex-col">
-                          <span>
-                            {regPrice} {translations[`currency${selectedCurrency}`]}
-                          </span>
-                          {el.registrar.currency !== selectedCurrency &&
-                            <small>
-                              {regPriceOriginal} {translations[`currency${el.registrar.currency}`]}
-                            </small>
-                          }
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        <div className="flex flex-col">
-                          <span>
-                            {renPrice} {translations[`currency${selectedCurrency}`]}
-                          </span>
-                          {el.registrar.currency !== selectedCurrency &&
-                            <small>
-                              {renPriceOriginal} {translations[`currency${el.registrar.currency}`]}
-                            </small>
-                          }
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        <div className="flex flex-col">
-                          <span>
-                            {totalPrice} {translations[`currency${selectedCurrency}`]}
-                          </span>
-                          {el.registrar.currency !== selectedCurrency &&
-                            <small>
-                              {totalPriceOriginal} {translations[`currency${el.registrar.currency}`]}
-                            </small>
-                          }
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a href="#" className="text-primary-600 hover:text-primary-900">
-                          <button onClick={() => handleAddToCart(el)}>
-                            <PlusIcon className="h-5 w-5" />
-                          </button>
-                        </a>
-                      </td>
-                    </tr>
-                  )
-                }
-                )}
-              </tbody>
-            </table>
-          </div>
-        </div>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
 
       <Pagination
