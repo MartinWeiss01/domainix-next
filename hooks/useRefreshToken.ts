3427cec5502c71722a9d3baf6ae9976f6820a813
Martin Weiss <jsem@martinweiss.cz>
2023-09-14 23:44:56 +0200

    fix(hooks): remove hook from condition
    This hook was breaking Rules of Hooks since hooks were conditionally called.
    This problem was fixed, also the hook was simplified and made to be more readable.
    It now uses globalFetcher, which will be later also used for Whois API.

--- hooks/useRefreshToken.ts
+++ hooks/useRefreshToken.ts
@@ -3,44 +3,31 @@ import { ITokenResponse } from "@/types/api/tokenEndpoint"
 import { useEffect } from "react"
 import useSWR from "swr"
 import Cookies from "js-cookie"
+import { globalFetcher } from "@/libs/globalSWRFetcher"
 
 const COOKIE_DAYS_EXPIRATION: number = 7
-const fetcher = (url: string): Promise<ITokenResponse> => fetch(url, { method: 'POST' }).then(res => res.json())
 
 export const useRefreshToken = (): { response: ITokenResponse | undefined; error: any; isLoading: boolean } => {
-  //Wait for window to be available (Client side only)
-  if (typeof window !== 'undefined') {
-    const token = Cookies.get(process.env.NEXT_PUBLIC_COOKIE_TOKEN_NAME)
+  const token = Cookies.get(process.env.NEXT_PUBLIC_COOKIE_TOKEN_NAME)
 
-    if (token === undefined) {
-      // If token is not available, fetch it
-      const { data, error, isLoading } = useSWR(
-        process.env.NEXT_PUBLIC_APPLICATION_URL + '/api/auth/createToken',
-        fetcher
-      )
+  // If token is not available, fetch it
+  const { data, error, isLoading } = useSWR<ITokenResponse>(
+    typeof window !== 'undefined' && token === undefined ?
+      [`${process.env.NEXT_PUBLIC_APPLICATION_URL}/api/auth/createToken`, { method: 'POST' }] : null,
+    ([url, args]) => globalFetcher(url, args),
+    { revalidateOnFocus: false, revalidateOnReconnect: false }
+  )
 
-      // Wait for isLoading to be changed, then set the token in cookie in case of success fetch 
-      useEffect(() => {
-        if (data?.success) {
-          Cookies.set(process.env.NEXT_PUBLIC_COOKIE_TOKEN_NAME, data.token, { expires: COOKIE_DAYS_EXPIRATION })
-        }
-      }, [isLoading])
-
-      return {
-        response: data,
-        error,
-        isLoading
-      }
-    } else {
-      // If token is available, return it
-      return { response: { success: true, token }, error: undefined, isLoading: false }
+  // Wait for isLoading to be changed, then set the token in cookie in case of success fetch 
+  useEffect(() => {
+    if (data?.success) {
+      Cookies.set(process.env.NEXT_PUBLIC_COOKIE_TOKEN_NAME, data.token, { expires: COOKIE_DAYS_EXPIRATION })
     }
-  }
+  }, [isLoading])
 
-  // If window is not available, return undefined values (since Next runs it on server-side first time)
   return {
-    response: undefined,
-    error: undefined,
-    isLoading: true
+    response: data ? data : (token ? { success: true, token } : undefined),
+    error,
+    isLoading
   }
 }
