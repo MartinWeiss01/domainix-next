9350508ccce5c5a6cc78a96afd0c4a5c2de7d579
Martin Weiss <jsem@martinweiss.cz>
2023-09-15 14:04:56 +0200

    ci(Whois): add AvailabilityChecker Component

--- types/api/tokenEndpoint.ts
+++ types/api/tokenEndpoint.ts
@@ -3,3 +3,6 @@ export type ITokenResponse = {
 } | {
   success: true; token: string
 }
+
+export type TWhoisStatusOptions = 'unknown' | 'reserved' | 'registered' | 'available'
+export type TWhoisAPIResponse = { status: TWhoisStatusOptions; score: number }
